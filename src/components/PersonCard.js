01369471b3dfc7da921b4b7742f165b69520ef52
Nuanjan <nuanjan.schluntz@gmail.com>
2021-10-26 23:58:33 -0400

    Change class to Function component with hook.

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -1,37 +1,27 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 
-class PersonCard extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { count: props.person.age };
-  }
-
-  render() {
-    const { firstName, lastName, age, hairColor } = this.props.person;
-    return (
-      <div
-        style={{
-          border: "solid black 1px",
-          margin: "5px auto",
-          padding: "10px",
-          width: "50%",
-        }}
-      >
-        <h1>
-          {lastName}, {firstName}
-        </h1>
-        <p>Age: {this.state.count}</p>
-        <p>Hair Color: {hairColor}</p>
-        <button
-          onClick={() => {
-            this.setState({count: parseInt(this.state.count)+ 1})
-          }}
-        >
-          Birthday Button for {firstName} {lastName}
-        </button>
-      </div>
-    );
-  }
-}
+const PersonCard = (props) => {
+  const {age} = props.person
+  const [count, setCount] = useState(parseInt(age));
+  return (
+    <div
+      style={{
+        border: "solid black 1px",
+        margin: "5px auto",
+        padding: "10px",
+        width: "50%",
+      }}
+    >
+      <h1>
+        {props.person.lastName}, {props.person.firstName}
+      </h1>
+      <p>Age: {count}</p>
+      <p>Hair Color: {props.person.hairColor}</p>
+      <button onClick={() => setCount(count + 1)}>
+        Birthday Button for {props.person.firstName} {props.person.lastName}
+      </button>
+    </div>
+  );
+};
 
 export default PersonCard;
